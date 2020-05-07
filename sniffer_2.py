9ed8e80e65e5d17ecc91f7d09b30458b9fa2a3b8
Shreyans Jain <shreyansj551@gmail.com>
2020-05-07 12:40:11 +0530

    change socket protocol, format output

--- sniffer_2.py
+++ sniffer_2.py
@@ -6,10 +6,15 @@ import socket,sys,struct
 
 # create a network socket using the default constructor
 try:
-  sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+  sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 except socket.error:
   print('Socket could not be created.')
   sys.exit(1)
+
+def get_mac_address(bytesString):
+  bytesString = map('{:02x}'.format, bytesString)
+  destination_mac = ':'.join(bytesString).upper()
+  return destination_mac
 
 # while loop runs infinitely to capture any incoming packets
 while True:
@@ -24,7 +29,7 @@ while True:
   ethernet_proto = socket.htons(ethernet_proto)
   data = raw_data[14:]
 
-  print('Ethernet frame:')
+  print('\nEthernet frame:')
   print('\tDestination: {}, Source: {}, Ethernet Protocol: {}'.format(destination_mac, src_mac, ethernet_proto))
 
    # analyse only IPv4 packets (I know IPv6 is the real deal but this should work for now)
@@ -40,9 +45,3 @@ while True:
     print('IPv4 packet:')
     print('\tVersion: {}, Header length: {}, TTL: {}'.format(version,header_len,ttl))
     print('\tProtocol: {}, Source: {}, Target: {}'.format(proto,src,target))
-
-
-def get_mac_address(bytesString):
-  bytesString = map('{:02x}'.format, bytesString)
-  destination_mac = ':'.join(bytesString).upper()
-  return destination_mac
