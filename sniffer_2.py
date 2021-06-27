c5e227948cfc5a31a9d1f52736342b39103e961a
nmsilva4 <86568829+nmsilva4@users.noreply.github.com>
2021-06-27 14:44:11 -0300

    Update sniffer_2.py
    Melhoria da apresentação dos resultados do sniffer, identificando corretamente os protocolos, por exemplo, em vez de exibir “Protocol 6”, exibir “Protocolo TCP”;
    Identificação e tratamento para protocolo ICMP;
    Melhoria no tratamento de protocolo TCP;
    Identificação e tratamento para protocolo UDP;
    Adicionar a capacidade de tratamento de pacotes IPV6;
    Salvar a saída em arquivo ou tela para facilitar futura análise (hoje a mesma é exibida apenas na console);

--- sniffer_2.py
+++ sniffer_2.py
@@ -4,44 +4,108 @@
 
 import socket,sys,struct
 
-# create a network socket using the default constructor
+# Cria um socket de rede utilizando funções nativas do python
 try:
-  sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+	sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 except socket.error:
-  print('Socket could not be created.')
-  sys.exit(1)
+	print('Socket could not be created.')
+	sys.exit(1)
 
 def get_mac_address(bytesString):
-  bytesString = map('{:02x}'.format, bytesString)
-  destination_mac = ':'.join(bytesString).upper()
-  return destination_mac
+	bytesString = map('{:02x}'.format, bytesString)
+	destination_mac = ':'.join(bytesString).upper()
+	return destination_mac
 
-# while loop runs infinitely to capture any incoming packets
+def get_constants(prefix):
+	# Cria um dicionário mapeando as constantes do módulo de socket para seus nomes
+	return dict( (getattr(socket, n), n)
+				for n in dir(socket)
+				if n.startswith(prefix)
+				)
+
+protocols = get_constants('IPPROTO_')
+
+with open("result.txt", "w") as file:
+	file.write("\n")
+
+# Enquanto o loop é executado infinitamente para capturar qualquer pacote de entrada
 while True:
 
-  # listen on port 65565
-  raw_data, address = sock.recvfrom(65565)
-  destination_mac, src_mac, ethernet_proto = struct.unpack('! 6s 6s H', raw_data[:14])
+	# Escuta na porta 65565
+	raw_data, address = sock.recvfrom(65565)
+	destination_mac, src_mac, ethernet_proto = struct.unpack('! 6s 6s H', raw_data[:14])
 
-  # packet parameters
-  destination_mac = get_mac_address(destination_mac)
-  src_mac = get_mac_address(src_mac)
-  ethernet_proto = socket.htons(ethernet_proto)
-  data = raw_data[14:]
+	# Parâmetros do pacote
+	destination_mac = get_mac_address(destination_mac)
+	src_mac = get_mac_address(src_mac)
+	ethernet_proto = socket.htons(ethernet_proto)
+	# try:
+	# 	ethernet_proto = protocols[ethernet_proto]
+	# except:
+	# 	ethernet_proto = "None"
+	data = raw_data[14:]
 
-  print('\nEthernet frame:')
-  print('\tDestination: {}, Source: {}, Ethernet Protocol: {}'.format(destination_mac, src_mac, ethernet_proto))
+	print('\nEthernet frame:')
+	print('\tDestination: {}, Source: {}, Ethernet Protocol: {}'.format(destination_mac, src_mac, ethernet_proto))
+	# Cria o arquivo result.txt
+	with open("result.txt", "a") as file:
+		file.write('\nEthernet frame:')
+		file.write('\tDestination: {}, Source: {}, Ethernet Protocol: {}'.format(destination_mac, src_mac, ethernet_proto))
 
-   # analyse only IPv4 packets (I know IPv6 is the real deal but this should work for now)
-  if (ethernet_proto == 8):
-    version_header_len = data[0]
-    version = version_header_len >> 4
-    header_len = (version_header_len & 15) * 4
-    ttl,proto,src,target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+	# Captura pacotes IPv4
+	if (ethernet_proto == 8):
+		version_header_len = data[0]
+		version = version_header_len >> 4
+		header_len = (version_header_len & 15) * 4
+		ttl,proto,src,target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+		# Captura o nome do protocolo
+		try:
+			proto = protocols[proto]
+			proto = proto.split("_")[1] + " Protocol"
+		except:
+			proto = "None Specified"
 
-    src = '.'.join(map(str,src)) 
-    target = '.'.join(map(str,target)) 
-    
-    print('IPv4 packet:')
-    print('\tVersion: {}, Header length: {}, TTL: {}'.format(version,header_len,ttl))
-    print('\tProtocol: {}, Source: {}, Target: {}'.format(proto,src,target))
+		src = '.'.join(map(str,src)) 
+		target = '.'.join(map(str,target)) 
+
+		print('IPv4 packet:')
+		print('\tVersion: {}, Header length: {}, TTL: {}'.format(version,header_len,ttl))
+		print('\t{}, Source: {}, Target: {}'.format(proto,src,target))
+
+		# Salva o resultado no arquivo result.txt
+		with open("result.txt", "a") as file:
+			file.write('\nIPv4 packet:')
+			file.write('\n\tVersion: {}, Header length: {}, TTL: {}'.format(version,header_len,ttl))
+			file.write(',{}, Source: {}, Target: {}\n'.format(proto,src,target))
+
+		# Captura pacotes IPv6
+		data = raw_data[14:]
+		version_header_len = data[0]
+		version = (version_header_len & 0xf0) >> 4
+		header_len = version_header_len & 0x0f
+		ttl,proto,src,target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+		# Captura o nome do protocolo
+		try:
+			proto = protocols[proto]
+			proto = proto.split("_")[1] + " Protocol"
+		except:
+			proto = "None Specified"
+		src = '.'.join(map(str,src)) 
+		numbers = list(map(int, src.split('.')))
+		source = '2002:{:02x}{:02x}:{:02x}{:02x}::'.format(*numbers)
+		target = '.'.join(map(str,target)) 
+		numbers = list(map(int, target.split('.')))
+		target = '2002:{:02x}{:02x}:{:02x}{:02x}::'.format(*numbers)
+		print('IPv6 packet:')
+		print('\tVersion: {}, Header length: {}, TTL: {}'.format(version, header_len, ttl))
+		print('\t{}, Source: {}, Target: {}'.format(proto, source, target))
+
+		# Salva o resultado no arquivo result.txt
+		with open("result.txt", "a") as file:
+			file.write('IPv6 packet:')
+			file.write('\n\tVersion: {}, Header length: {}, TTL: {}'.format(version, header_len, ttl))
+			file.write(',{}, Source: {}, Target: {}'.format(proto, source, target))
+
+	print()
+	with open("result.txt", "a") as file:
+		file.write("\n")
